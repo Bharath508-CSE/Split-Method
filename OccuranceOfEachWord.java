e577c655fc79dc1b73b2fc425b26d150bcfe338b
Bharath <bharath23695a0508@gmail.com>
2026-05-28 16:08:41 +0530

    Occurance of Each word in given String

---
+++ OccuranceOfEachWord.java
@@ -0,0 +1,25 @@
+class OccuranceOfEachWord {
+    public static void main(String[] args) {
+      String s="java is a very very easy easy programming";
+      String[] str=s.split(" ");
+      for(int i=0;i<str.length;i++)
+      {
+          if(str[i]==" ")
+          {
+              continue;
+          }
+          int count=1;
+          for(int j=i+1;j<str.length;j++)
+          {
+              
+              if(str[i].equals(str[j]))
+              {
+                  count++;
+                  str[j]=" ";
+              }
+          }
+          System.out.println(str[i]+" = "+count);
+      }
+    }
+    
+}
